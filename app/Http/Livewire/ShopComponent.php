eddce4342fdaea2d8eee52bc68b667218e5d5e5a
abhishek singh <abhi212068@gmail.com>
2024-01-08 22:51:45 +0530

    add remove wishlist items functionality

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -43,6 +43,17 @@ class ShopComponent extends Component
         $this->emitTo('wishlist-icon-component', 'refreshComponent');
     }
 
+    public function removeFromwishlist($product_id)
+    {
+        foreach (Cart::instance('wishlist')->content() as $witem) {
+            if ($witem->id == $product_id) {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-icon-component', 'refreshComponent');
+                return;
+            }
+        }
+    }
+
     public function render()
     {
         if ($this->orderBy == 'Price: Low to High') {
