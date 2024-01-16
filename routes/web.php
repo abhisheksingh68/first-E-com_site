5d95dd90c5df5e69b43d97aebfa8f73c1117a134
abhishek singh <abhi212068@gmail.com>
2024-01-16 22:11:50 +0530

    add show wishlist and remove function

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\SearchComponent;
+use App\Http\Livewire\WishlistComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,6 +35,8 @@ Route::get('/shop', ShopComponent::class)->name('shop');
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 Route::get('/cart', CartComponent::class)->name('shop.cart');
+
+Route::get('/wishlist', WishlistComponent::class)->name('shop.wishlist');
 
 
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
