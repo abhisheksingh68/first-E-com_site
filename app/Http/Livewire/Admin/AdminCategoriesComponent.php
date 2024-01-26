2b0d006d7e24b2c7ae301eac92283f43d709554e
abhishek singh <abhi212068@gmail.com>
2024-01-26 23:37:12 +0530

    admin delete category

--- app/Http/Livewire/Admin/AdminCategoriesComponent.php
+++ app/Http/Livewire/Admin/AdminCategoriesComponent.php
@@ -8,7 +8,16 @@ use Livewire\WithPagination;
 
 class AdminCategoriesComponent extends Component
 {
+    public $category_id;
+
     use WithPagination;
+    public function deteleCategory()
+    {
+        $category = Category::find($this->category_id);
+        $category->delete();
+        session()->flash('message', 'Category has been deleted successfully !');
+    }
+
     public function render()
     {
         $categories = Category::orderBy('name', 'ASC')->paginate(5);
--- resources/views/livewire/admin/admin-categories-component.blade.php
+++ resources/views/livewire/admin/admin-categories-component.blade.php
@@ -34,6 +34,9 @@
                                 </div>
                             </div>
                             <div class="card-body">
+                                @if(Session::has('message'))
+                                <div class="alert alert-success" role="" alert>{{Session::get('message')}}</div>
+                                @endif
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
@@ -57,6 +60,7 @@
                                             <td>{{$category->slug}}</td>
                                             <td>
                                                 <a href="{{route('admin.category.edit',['category_id'=>$category->id])}}" class="text-info">Edit</a>
+                                                <a href="#" class="text-danger" onclick="deleteConfirmation({{$category->id}})" style="margin-left:20px;">Delete</a>
                                             </td>
                                         </tr>
                                         @endforeach
@@ -71,3 +75,33 @@
         </section>
     </main>
 </div>
+
+<div class="modal" id="deleteConfirmation">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body pb-30 pt-30">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h4 class="pb-3">Do you want to delete this record ?</h4>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Cancel</button>
+                        <button type="button" onclick="deleteCategory()" class="btn btn-danger">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+<script>
+    function deleteConfirmation(id) {
+        @this.set('category_id', id);
+        $('#deleteConfirmation').modal('show');
+    }
+
+    function deleteCategory() {
+        @this.call('deteleCategory');
+        $('#deleteConfirmation').modal('hide');
+    }
+</script>
+@endpush
